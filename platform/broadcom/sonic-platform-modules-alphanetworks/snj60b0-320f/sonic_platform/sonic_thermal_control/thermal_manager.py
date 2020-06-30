a96cf3cc96bff036cbe0b1484e44dae001014fca
Tina Tseng <tina_tseng@alphanetworks.com>
2020-06-30 15:44:02 +0800

    modify for alert "Import pollutes the enclosing namespace"

--- platform/broadcom/sonic-platform-modules-alphanetworks/snj60b0-320f/sonic_platform/sonic_thermal_control/thermal_manager.py
+++ platform/broadcom/sonic-platform-modules-alphanetworks/snj60b0-320f/sonic_platform/sonic_thermal_control/thermal_manager.py
@@ -1,7 +1,4 @@
 from sonic_platform_base.sonic_thermal_control.thermal_manager_base import ThermalManagerBase
-from .thermal_actions import *
-from .thermal_conditions import *
-from .thermal_infos import *
 
 
 class ThermalManager(ThermalManagerBase):
